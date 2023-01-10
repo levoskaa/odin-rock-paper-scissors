32d193692724a81fda9b826cf08b852e9a23e00e
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-01-10 18:50:52 +0100

    feat: extend game function with logging and scores

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
-const ROCK = 'Rock';
-const PAPER = 'Paper';
-const SCISSORS = 'Scissors';
+const CHOICE = {
+  ROCK: 'Rock',
+  PAPER: 'Paper',
+  SCISSORS: 'Scissors',
+};
+const ROUND_RESULT = {
+  PLAYER_WON: 'playerWon',
+  COMPUTER_WON: 'computerWon',
+  TIE: 'tie',
+};
 
 function game() {
+  let playerWins = 0;
+  let computerWins = 0;
+
   for (let i = 0; i < 5; i++) {
     const playerSelection = prompt(
       'Enter your choice: rock, paper or scissors!'
     );
-    const result = playRound(playerSelection, getComputerChoice());
-    console.log(result);
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+
+    logRoundResult(
+      result,
+      capitalize(playerSelection.toLowerCase()),
+      computerSelection
+    );
+    if (result === ROUND_RESULT.PLAYER_WON) {
+      playerWins++;
+    } else if (result === ROUND_RESULT.COMPUTER_WON) {
+      computerWins++;
+    }
   }
+
+  logFinalResult(playerWins, computerWins);
 }
 
 function getComputerChoice() {
-  const possibleChoices = [ROCK, PAPER, SCISSORS];
+  const possibleChoices = Object.values(CHOICE);
   const randomIndex = Math.floor(Math.random() * possibleChoices.length);
   return possibleChoices[randomIndex];
 }
@@ -22,26 +45,42 @@ function playRound(playerSelection, computerSelection) {
   // Convert to uppercase to check case-insensitively
   playerSelection = playerSelection.toUpperCase();
   if (playerSelection === computerSelection.toUpperCase()) {
-    return 'Tie!';
+    return ROUND_RESULT.TIE;
   }
 
   let hasPlayerWon;
   switch (playerSelection) {
-    case ROCK.toUpperCase():
-      hasPlayerWon = computerSelection === SCISSORS;
+    case CHOICE.ROCK.toUpperCase():
+      hasPlayerWon = computerSelection === CHOICE.SCISSORS;
       break;
-    case PAPER.toUpperCase():
-      hasPlayerWon = computerSelection === ROCK;
+    case CHOICE.PAPER.toUpperCase():
+      hasPlayerWon = computerSelection === CHOICE.ROCK;
       break;
-    case SCISSORS.toUpperCase():
-      hasPlayerWon = computerSelection === PAPER;
+    case CHOICE.SCISSORS.toUpperCase():
+      hasPlayerWon = computerSelection === CHOICE.PAPER;
       break;
   }
 
-  playerSelection = capitalize(playerSelection.toLowerCase());
-  return hasPlayerWon
-    ? `You Win! ${playerSelection} beats ${computerSelection}`
-    : `You Lose! ${computerSelection} beats ${playerSelection}`;
+  return hasPlayerWon ? ROUND_RESULT.PLAYER_WON : ROUND_RESULT.COMPUTER_WON;
+}
+
+function logRoundResult(result, playerSelection, computerSelection) {
+  let textResult;
+  switch (result) {
+    case ROUND_RESULT.PLAYER_WON:
+      textResult = `You Win! ${playerSelection} beats ${computerSelection}`;
+      break;
+    case ROUND_RESULT.COMPUTER_WON:
+      textResult = `You Lose! ${computerSelection} beats ${playerSelection}`;
+      break;
+    default:
+      textResult = 'Tie!';
+  }
+  console.log(textResult);
+}
+
+function logFinalResult(playerWins, computerWins) {
+  console.log(`Final result - You: ${playerWins}\tComputer: ${computerWins}`);
 }
 
 function capitalize(word) {
