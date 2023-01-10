18f67f767a13498eff3af7b14c1f87e28d455390
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-01-10 17:46:53 +0100

    feat: implement playRound function

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
+const ROCK = 'Rock';
+const PAPER = 'Paper';
+const SCISSORS = 'Scissors';
+
 function getComputerChoice() {
-  const possibleChoices = ['Rock', 'Paper', 'Scissors'];
+  const possibleChoices = [ROCK, PAPER, SCISSORS];
   const randomIndex = Math.floor(Math.random() * possibleChoices.length);
   return possibleChoices[randomIndex];
+}
+
+function playRound(playerSelection, computerSelection) {
+  // Convert to uppercase to check case-insensitively
+  playerSelection = playerSelection.toUpperCase();
+  if (playerSelection === computerSelection.toUpperCase()) {
+    return 'Tie!';
+  }
+
+  let hasPlayerWon;
+  switch (playerSelection) {
+    case ROCK.toUpperCase():
+      hasPlayerWon = computerSelection === SCISSORS;
+      break;
+    case PAPER.toUpperCase():
+      hasPlayerWon = computerSelection === ROCK;
+      break;
+    case SCISSORS.toUpperCase():
+      hasPlayerWon = computerSelection === PAPER;
+      break;
+  }
+
+  playerSelection = capitalize(playerSelection.toLowerCase());
+  return hasPlayerWon
+    ? `You Win! ${playerSelection} beats ${computerSelection}`
+    : `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1);
 }
